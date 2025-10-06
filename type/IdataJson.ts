e299d1e00a4d52276e1533e0637122c0f1553aad
Bogdoing <Bogdoingg@gmail.com>
2025-10-06 21:59:50 +0300

    rewrite type class

--- type/IdataJson.ts
+++ type/IdataJson.ts
@@ -1,9 +1,21 @@
-export interface ChartItem {
-    l_id: number
-    reg_id: number
+export interface GradeData {
     v: number
     vr: number
     r: number
+}
+
+export interface ChartItem {
+    l_id: number
+    reg_id: number
+    v?: number
+    vr?: number
+    r?: number
+    grades?: {
+        "0": GradeData
+        "1-3": GradeData
+        "3-6": GradeData
+        "6+": GradeData
+    }
     d: string
 }
 
